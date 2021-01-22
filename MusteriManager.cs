53c135937978d2b3ac4a3fa3b2f1a8df6ef01029
atacan <atacan@LENOVO-PC>
2021-01-22 23:24:36 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        
+
+
+        public void Add (Musteri musteri1)
+        {
+                   Console.WriteLine("hoşgeldiniz" + " " + "sayin" + " " + musteri1.Adi + " " + musteri1.Soyadi);
+        }
+
+        public void Delete(Musteri musteri1)
+        {           
+            Console.WriteLine("sayin" + " " + musteri1.Adi + " " + musteri1.Soyadi + " " + "kaydiniz basariyle silinmistir.");
+        }
+
+        public void List(Musteri[] musteriler)
+        {
+
+            foreach(Musteri List in musteriler)
+
+            {
+
+                Console.WriteLine( List.Adi + " " +  List.Soyadi + " " +  List.Yas + " " + List.Id);
+            }
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           
+        
+            string[] musteri = new string[] { };
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "atacan";
+            musteri1.Soyadi = "erdoğan";
+            musteri1.Yas = 25;
+            musteri1.Id = 111;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "can";
+            musteri2.Soyadi = "kaya";
+            musteri2.Yas = 2;
+            musteri2.Id = 12;
+
+            MusteriManager x = new MusteriManager();
+            x.Add(musteri1);
+
+            x.Delete(musteri2);
+
+            Musteri[] y = new Musteri[] { musteri1, musteri2 };
+
+            x.List(y);
+
+        
+        }
+    }
+}
